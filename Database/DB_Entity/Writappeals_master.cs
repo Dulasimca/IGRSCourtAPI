6c87bb45fe70f8d72d2c540722116e7ff5f935c3
dhanarthi <arthivenkat87@gmail.com>
2022-12-12 14:42:52 +0530

    Adding DB entity file Writappeals_master.cs

---
+++ Database/DB_Entity/Writappeals_master.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IGRSCourtAPI.Database.DB_Entity
+{
+    [Table("writappeals_master")]
+    public class Writappeals_master
+    {
+
+        public Writappeals_master()
+        {
+            // District_masters = new HashSet<District_master>();
+            //Sro_masters = new HashSet<Sro_master>();
+            //Zone_masters = new.HashSet<Zone_master>();
+            // Courtcases = new HashSet<Courtcase>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int writappealsid { get; set; }
+        public int zoneid { get; set; }
+        public int districtid { get; set; }
+        public int sroid { get; set; }
+        public int courtcaseid { get; set; }
+        public string regularnumber { get; set; }
+        public string natureofdisposal { get; set; }
+        public string remarks { get; set; }
+        public DateTime? createddate { get; set; }
+        public bool flag { get; set; }
+
+        //public virtual ICollection<District_master> District_masters { get; set; }
+
+        //public virtual ICollection<Sro_master> Sro_masters { get; set; }
+
+        //public virtual ICollection<Zone_master> Zone_masters { get; set; }
+
+        //public virtual ICollection<Courtcase> Courtcases { get; set; }
+
+    }
+}
